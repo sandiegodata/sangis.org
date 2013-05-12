c2606018457a6a9d023528b84766ce3d9a3c007e
Eric Busboom <eric@clarinova.com>
2013-05-12 06:51:05 -0700

    Moving bundles to new locations

--- sangis.org-parcels-orig/bundle.py
+++ sangis.org-parcels-orig/bundle.py
@@ -11,178 +11,38 @@ class Bundle(BuildBundle):
         self.super_ = super(Bundle, self)
         self.super_.__init__(directory)
  
-    def meta(self):
-        
-        # To generate the schema, must have the table already loaded
-        with open (self.filesystem.path('meta','orig_schema.csv'), 'wb') as f:
-            self.schema.as_csv(f)
-        
-        return True
- 
+    def get_zone(self):
+        """Return the State Plane zone, and the SRS, for San Diego. """
+        _, places = self.library.dep('places')
+        sd = places.query("SELECT * FROM places where code = 'SD' LIMIT 1").first()
+        return sd['spsrs']
+    
     def build(self):
         from databundles.identity import PartitionIdentity
 
+        
+        srs = self.get_zone()
+
         for name, url in self.config.build.sources.items():
-            pid = PartitionIdentity(self.identity, table=name+'_orig')
-            p = self.partitions.new_geo_partition(pid, shape_file=url)
-
-
-        self.add_indexes()
-
-        self.split_docs()
+            pid = PartitionIdentity(self.identity, table=name)
+            p = self.partitions.new_geo_partition(pid, shape_file=url, t_srs=srs)
         
-        self.add_places()
-        
+        with open (self.filesystem.path('meta','orig_schema.csv'), 'wb') as f:
+            self.schema.as_csv(f)
+
+        p = self.partitions.find(table='parcels')
+        p.database.query('CREATE INDEX IF NOT EXISTS apn_idx ON parcels (apn, apn_8);')
+        p.database.query('CREATE INDEX IF NOT EXISTS parcel_idx ON parcels (parcelid);')
+        p.database.query('CREATE INDEX IF NOT EXISTS road_idx ON parcels (addrno, roadname);')
+        p.database.query('CREATE INDEX IF NOT EXISTS sroad_idx ON parcels (situs_addr, situs_stre);')
+        p.database.query('CREATE INDEX IF NOT EXISTS zip_idx ON parcels (zip);')
+        p.database.query('CREATE INDEX IF NOT EXISTS szip_idx ON parcels (situs_zip);')
+        p.database.query('CREATE INDEX IF NOT EXISTS jur_idx ON parcels (situs_juri);')
+        p.database.query('CREATE INDEX IF NOT EXISTS coord_idx ON parcels (x_coord, y_coord);')
+
 
         return True
 
-    def get_zone(self):
-        """Return the State Plane zone, and the SRS, for San Diego. """
-        
-        _, places = self.library.dep('places')
-        
-        
-        sd = places.query("SELECT * FROM places where code = 'SD' LIMIT 1").first()
-        
-        return sd['spsrs']
-  
-    def split_docs(self):
-        """Split the original parcel records into parcels, with a few extra fields, and parcel docs, 
-        with just the situs and assessors information"""
-        
-        parcels = self.partitions.find_or_new_geo(table='parcels', space='all')
-        parceldocs = self.partitions.find_or_new(table='parceldocs', space='all')
-        parcels_o = self.partitions.find(table='parcels_orig')
-        
-        lr = self.init_log_rate(2000)
-        
-        p_ins =  parcels.database.inserter()
-        pd_ins =  parceldocs.database.inserter()
-             
-        # Get the stateplane SRS number
-        spsrs = self.get_zone()
-                
-        for row in parcels_o.query(""" SELECT *, AsText(Transform(geometry, {spsrs})) as wkt FROM parcels_orig""".format(spsrs=spsrs)):
-        
-            row = dict(row)
-        
-            lr("Copy parcels")
-            
-            p_ins.insert(row)
-            pd_ins.insert(row)            
-        
-        
-        # Do this  seperately because they all depend on the geomoetry being tansformed in the previous query
-        parcels.query("""
-        UPDATE parcels SET 
-            area = Area(geometry),
-            x = X(Centroid(geometry)), y = Y(Centroid(geometry)),
-            lon = X(Transform(Centroid(geometry), 4326)), lat = Y(Transform(Centroid(geometry), 4326));
-        """)
-    
-
-    def add_places(self):      
-        from databundles.geo.util import segment_points
-        
-        parcels = self.partitions.find_or_new(table='parcels', space='all')
-        
-        # Spatialite takes over the primary key
-        #parcels.query("UPDATE parcels SET parcels_id = objectid");
-        #parcels.query('CREATE INDEX IF NOT EXISTS parcels_id_idx ON parcels (parcels_id);')
-        
-        lr = self.init_log_rate(1000)
-        
-        _, places = self.library.dep('places')
-        
-        for area, where, is_in in segment_points(places, 
-                                                 "SELECT *, AsText(geometry) AS wkt FROM places ORDER BY area ASC"):
-
-            count = 0;
-            self.log("Area {}".format(area['name']))
-            with parcels.database.updater() as upd:
-                for parcel in parcels.query("SELECT *, AsText(geometry) as wkt FROM parcels  WHERE {} AND {} IS NULL ".format(where, area['type'])):
-                    count +=1
-                    
-                    parcel = dict(parcel)
-
-                    if is_in(parcel['lon'], parcel['lat']):
-                        lr("Update {} {}".format(area['type'], area['name']))
-                        upd.update({'_OGC_FID': parcel['OGC_FID'],
-                               '_'+area['type'] : area['code']
-                               })
-
-
-            self.log("{} {}: {}".format(area['type'], area['name'], count))
-
-    def add_indexes(self):
-
-        p = self.partitions.find(table='parcels_orig')
-        p.database.query('CREATE INDEX IF NOT EXISTS apn_idx ON parcels_orig (apn, apn_8);')
-        p.database.query('CREATE INDEX IF NOT EXISTS parcel_idx ON parcels_orig (parcelid);')
-        p.database.query('CREATE INDEX IF NOT EXISTS road_idx ON parcels_orig (addrno, roadname);')
-        p.database.query('CREATE INDEX IF NOT EXISTS sroad_idx ON parcels_orig (situs_addr, situs_stre);')
-        p.database.query('CREATE INDEX IF NOT EXISTS zip_idx ON parcels_orig (zip);')
-        p.database.query('CREATE INDEX IF NOT EXISTS szip_idx ON parcels_orig (situs_zip);')
-        p.database.query('CREATE INDEX IF NOT EXISTS jur_idx ON parcels_orig (situs_juri);')
-        p.database.query('CREATE INDEX IF NOT EXISTS coord_idx ON parcels_orig (x_coord, y_coord);')
-             
-    def split_places(self):
-        "Break the all parcels file into seperate files by city, neighborhood, etc. "
-        _, places = self.library.dep('places')
-        all_parcels = self.partitions.find(table='parcels', space='all')
-        lr = self.init_log_rate(1000)
-        for place in places.query("SELECT * FROM places"):
-
-            split_parcels = self.partitions.find_or_new_geo(table='parcels', space=place['code'])
-            
-            with split_parcels.database.inserter() as ins:
-                for parcel in all_parcels.query(
-                        "SELECT *, AsText(geometry) as wkt FROM parcels WHERE {} = ?".format(place['type']), 
-                        place['code']):
-                  
-                    lr('Split parcels {} {}'.format(place['type'], place['code']))
-                    ins.insert(parcel)
-            
-            
-    def group_parcels(self):
-        import osr, ogr
-            
-        groups = set()
-        
-        all_parcels = self.partitions.find(table='parcels', space='all')
-        where_template = ""
-        for p1 in all_parcels.query("""SELECT *, AsText(geometry) AS wkt  FROM parcels WHERE asr_zone = 6 ORDER BY area"""):
-            p1g = ogr.CreateGeometryFromWkt(p1['wkt'])
-            p1b = p1g.Buffer(1000)
-            e = p1b.GetEnvelope()
-        
-            count = 0
-            
-            q = """
-                SELECT * FROM parcels 
-                WHERE asr_zone = 6 AND OGC_FID != {id}
-                AND x BETWEEN {x1} AND {x2} AND y BETWEEN {y1} and {y2}
-                ORDER BY area""".format(id=p1['OGC_FID'], x1=e[0], x2=e[1], y1=e[2], y2=e[3])
-            
-            self.log(q)
-            
-            for p2 in all_parcels.query(q):
-                
-                count += 1
-                p2g = ogr.CreateGeometryFromWkt(p1['wkt'])
-                
-                if p1g.Distance(p2g) < 300:
-                    id1  = p1g.OGC_FID
-                    id2  = p2g.OGC_FID
-                    
-                    if id1 > id2:
-                        id1, id2 = id2, id1
-                    
-                    groups.add( (id1, id2) )
-                    
-            self.log("Checked {} Groups: {}".format(count, len(groups)))
-                    
-        
 import sys
 
 if __name__ == '__main__':
--- sangis.org-places-orig/bundle.py
+++ sangis.org-places-orig/bundle.py
@@ -19,151 +19,22 @@ class Bundle(BuildBundle):
             self.schema.as_csv(f)
             
         return True
+    
 
 
     def build(self):
         from databundles.identity import PartitionIdentity
 
+        source_srs = self.config.build.source_srs
+
         for name, url in self.config.build.sources.items():
-            table_name = name+'_orig'
-            pid = PartitionIdentity(self.identity, table=table_name)
-            
+            pid = PartitionIdentity(self.identity, table=name)
             p = self.partitions.new_geo_partition(pid, shape_file=url)
-
-
-    
-        self.build_places()
-
-        self.schema.add_views()
-        
-        self.add_spcs()
 
         return True
 
-    def build_places(self):
-
-        col_map = self.filesystem.read_csv(self.filesystem.path('meta','colmap.csv'), ('table', 'outcol'))
-
-        placep = self.partitions.find_or_new_geo(table='places')
-
-        try: placep.query("DELETE FROM places")
-        except: pass
-
-            
-        # The original files define the SRS directly, so lets make that an 
-        # authoritative value
-        source_srs = self.config.build.source_srs
 
 
-        with placep.database.inserter('places') as ins:
-            for p in self.partitions:
-                
-                if not p.identity.name.endswith('orig'):
-                    continue
-                
-                table = str(p.identity.table)
-                city = None
-                
-                mapping = col_map.get( (table,"name"), False )
-                group_name = mapping['group']
-                
-                for i, row in enumerate(p.database.query("""
-                SELECT *,
-                AsText(Transform(geo, 4326)) AS wkt,
-                X(Transform(Centroid(geo), 4326)) AS lon, 
-                Y(Transform(Centroid(geo), 4326)) as lat,
-                Area(geo) as areax,
-                AsText(Envelope(geo)) as envelope
-                FROM ( SELECT *, CastToMultiPolygon(GUnion(Buffer(geometry,0.0))) as geo FROM {} GROUP BY {})
-                """.format(table, group_name))):
-                    drow = dict(row)
-
-                    drow['origsrs'] = source_srs
-
-                    for oc in ('code','name'):
-                        mapping = col_map.get( (table,str(oc)), False )
-    
-                        ic = mapping['incol']
-                        
-                        if '{' in ic:
-                            v = ic.format(i=i,**drow)
-                        else:
-                            v = drow[ic]
-                        
-                        drow[oc] = v
-    
-                    drow['area'] = drow['areax']
-                     
-                    drow['name'] = drow['name'].title()
-                    
-                    drow['type'] = table.replace('_orig','')
-                    
-                    if '_' in drow['type']:
-                        city, type_ = drow['type'].split('_',2)
-                    else:
-                        city = None
-                        type_ = drow['type']
-                        
-                    drow['type'] = type_
-                    drow['city'] = city    
-
-                        
-                    ins.insert(drow)
-
-  
-    def add_spcs(self):
-        """Attach the SPCS zones ( Which will always be California 6 ) and the envelopes """
-        import ogr, osr
-        import json
-        
-        from databundles.geo.util import segment_points
-        from databundles.geo.analysisarea import AnalysisArea
-        
-        temp = self.partitions.find(grain='temp', tables=('places'))
-
-        _, zones = self.library.dep('spzones')
-
-        places = self.partitions.find(table='places')
-        placessrs = places.get_srs()
-
-      # Spatiaite takes over the primary key in geo partitions. 
-        places.query("UPDATE places SET places_id = OGC_FID")
-
-
-
-        for zone, where, is_in in segment_points(zones, 
-                                        "SELECT *, AsText(geometry) AS wkt FROM spzones",
-                                        "lon BETWEEN {x1} AND {x2} AND lat BETWEEN {y1} and {y2}"):
-
-            with places.database.updater() as upd:
-                for place in places.query("SELECT *, AsText(geometry) AS wkt  FROM places WHERE {}".format(where)):
-    
-                    if is_in(place['lon'], place['lat']):
-                        self.log("Attach {} to {}".format(place['name'], zone['name']))
-                    
-                        spsrs =  ogr.osr.SpatialReference()
-                        spsrs.ImportFromEPSG(zone['srid'])
-                        
-                        transform = osr.CoordinateTransformation(placessrs,spsrs)   
-                        g = ogr.CreateGeometryFromWkt(place['wkt'])
-                        wgsenvelope = g.GetEnvelope()
-                        g.Transform(transform)
-                        utmenvelope = g.GetEnvelope()
-
-                        aa = AnalysisArea.new_from_envelope(spsrs, utmenvelope, scale=1)
-                        aa.name = place['name']
-                        aa.geoid = place['code']
-                      
-                        
-                        u = {
-                             '_OGC_FID': place['OGC_FID'],
-                             '_spsrs': zone['srid'],
-                             '_wgsenvelope' : json.dumps(aa.ll_envelope),
-                             '_utmenvelope' : json.dumps(aa.ne_envelope),
-                             '_aa' : aa.to_json()
-                            }
-          
-                        upd.update(u)
     
 import sys
 
