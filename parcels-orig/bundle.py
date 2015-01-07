921edc6272d47e4eac31d53b83f2dc69ebde5282
Eric Busboom <eric@clarinova.com>
2015-01-06 20:05:58 -0800

    .

--- parcels-orig/bundle.py
+++ parcels-orig/bundle.py
@@ -15,13 +15,13 @@ class Bundle(GeoBuildBundle):
         
         pdq = p.database.query
         
-        pdq('CREATE INDEX IF NOT EXISTS apn_idx ON parcels (apn, apn_8);')
-        pdq('CREATE INDEX IF NOT EXISTS parcel_idx ON parcels (parcelid);')
-        pdq('CREATE INDEX IF NOT EXISTS road_idx ON parcels (addrno, roadname);')
-        pdq('CREATE INDEX IF NOT EXISTS sroad_idx ON parcels (situs_addr, situs_stre);')
-        pdq('CREATE INDEX IF NOT EXISTS zip_idx ON parcels (zip);')
-        pdq('CREATE INDEX IF NOT EXISTS szip_idx ON parcels (situs_zip);')
-        pdq('CREATE INDEX IF NOT EXISTS jur_idx ON parcels (situs_juri);')
-        pdq('CREATE INDEX IF NOT EXISTS coord_idx ON parcels (x_coord, y_coord);')
+        #pdq('CREATE INDEX IF NOT EXISTS apn_idx ON parcels (apn, apn_8);')
+        #pdq('CREATE INDEX IF NOT EXISTS parcel_idx ON parcels (parcelid);')
+        #pdq('CREATE INDEX IF NOT EXISTS road_idx ON parcels (addrno, roadname);')
+        #pdq('CREATE INDEX IF NOT EXISTS sroad_idx ON parcels (situs_addr, situs_stre);')
+        #pdq('CREATE INDEX IF NOT EXISTS zip_idx ON parcels (zip);')
+        #pdq('CREATE INDEX IF NOT EXISTS szip_idx ON parcels (situs_zip);')
+        #pdq('CREATE INDEX IF NOT EXISTS jur_idx ON parcels (situs_juri);')
+        #pdq('CREATE INDEX IF NOT EXISTS coord_idx ON parcels (x_coord, y_coord);')
 
         return True
